cd4d66aa016e0c954e7e23669b43169e112c9987
Dmitry <fantastico@live.ru>
2019-01-21 14:08:31 +0200

    Updates

---
+++ precache-manifest.9b3058d970272fbb4abd36f5d30a4208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9db9ebcdcbe640af21f6",
+    "url": "/cubes-react/static/css/main.deacc6c2.chunk.css"
+  },
+  {
+    "revision": "9db9ebcdcbe640af21f6",
+    "url": "/cubes-react/static/js/main.9db9ebcd.chunk.js"
+  },
+  {
+    "revision": "b664d0bb2efcb082c958",
+    "url": "/cubes-react/static/js/1.b664d0bb.chunk.js"
+  },
+  {
+    "revision": "ec360283096d39bd89d8",
+    "url": "/cubes-react/static/js/runtime~main.ec360283.js"
+  },
+  {
+    "revision": "e723d5864d3252e630e36cd3e2b15e7d",
+    "url": "/cubes-react/index.html"
+  }
+];
